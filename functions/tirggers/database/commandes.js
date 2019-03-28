24edb18f5ee1f471f67b6348dfd1bc2807768aec
chenineazeddine <chenineazeddine5@gmail.com>
2019-03-28 02:17:11 +0100

    adding order update FCM notification trigger

--- functions/tirggers/database/commandes.js
+++ functions/tirggers/database/commandes.js
@@ -24,16 +24,76 @@ const updateCarState = (id_marque,id_vehicule,state)=>{
 } 
 
 
+const sendNotification = (payload,uid)=>{
+        
+        return admin.firestore().collection("automobilistes")
+                     .doc(uid)
+                     .get()
+                     .then(doc => {
+                        let token = doc.data().instance_token
+                        return admin.messaging().sendToDevice(token, payload);
+                     })
+                     .catch((err)=>{
+                         console.log(err);
+                         return 0;
+                     })
+}
+
+
 const onOrderCreated = functions.firestore.document('commandes/{id_commande}')
       .onCreate((snap, context) => {
             console.log("onOrderCreated trigger");
             const order = snap.data();
             return updateCarState(order.id_marque,order.id_vehicule,false)
        });
+       
 
 const onOrderUpdated = functions.firestore.document('commandes/{id_commande}')
       .onUpdate((change, context) => {
-            console.log("onOrderUpdateTrigger");
+              
+
+              const ORDER_REJECTED = 0
+              const ORDER_ACCEPTED = 2
+              const order = change.after.data();
+              const oldData = change.before.data();
+              const notification = {
+                      title:"",
+                      body:""
+              }
+             
+              if(order.etas == oldData.etas) return //the order state didn't change
+              switch (order.etas) {
+                    case ORDER_REJECTED:
+                            notification.title = "Commande rejetée"
+                            break;
+                    case ORDER_ACCEPTED:
+                            notification.title = "Commande validée"
+                            break;
+                    default:
+                           return
+              }
+
+              notification.body = order.message
+              return  admin.firestore().collection("versions")
+                           .doc(order.id_version)
+                           .get()
+                           .then(doc => {
+                                        let version = doc.data()
+                                        return sendNotification({ notification,
+                                                data: {
+                                                    version: version.nom,
+                                                    price: String(order.prix),
+                                                    date: order.date.toDate().toString(),
+                                                    photo: version.url,
+                                                }},order.id_automobiliste)
+                                        
+                           })
+                           .then(()=>{
+                                   if(order.etas == ORDER_REJECTED)
+                                        return updateCarState(order.id_marque,order.id_vehicule,true)
+                                   else 
+                                        return
+                           })  
       });
 
 module.exports ={
